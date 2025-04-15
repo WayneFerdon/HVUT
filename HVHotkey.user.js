1c0bede640007845deb021b3a52ef4bbb97d288c
WayneFerdon <wayneferdon@hotmail.com>
2025-04-15 23:29:23 +0800

    Update HVHotkey.user.js

--- HVHotkey.user.js
+++ HVHotkey.user.js
@@ -2,7 +2,7 @@
 // @name         HVHotkey
 // @version      2025-04-15
 // @author       WayneFerdon
-// @include      http*://*.hentaiverse.org/*
+// @include      http*://*hentaiverse.org/*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @updateURL https://github.com/WayneFerdon/HVUT/raw/refs/heads/main/HVHotkey.user.js
 // ==/UserScript==
