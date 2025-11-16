9c300b291b57a3b7017f72f53823bf2da003fcc4
WayneFerdon <wayneferdon@hotmail.com>
2025-11-16 22:26:12 +0800

    Update version and improve UI element handling

--- HVAA_fit_HVUT.user.js
+++ HVAA_fit_HVUT.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         HVAA_fit_HVUT
-// @version      2025-04-15
+// @version      2025-11-16
 // @author       WayneFerdon
 // @include      http*://*hentaiverse.org/*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
@@ -8,19 +8,43 @@
 // ==/UserScript==
 
 function gE(ele, mode, parent) { // 获取元素
-    if (typeof ele === 'object') {
-        return ele;
-    } if (mode === undefined && parent === undefined) {
-        return (isNaN(ele * 1)) ? document.querySelector(ele) : document.getElementById(ele);
-    } if (mode === 'all') {
-        return (parent === undefined) ? document.querySelectorAll(ele) : parent.querySelectorAll(ele);
-    } if (typeof mode === 'object' && parent === undefined) {
-        return mode.querySelector(ele);
-    }
+  if (typeof ele === 'object') {
+    return ele;
+  } if (mode === undefined && parent === undefined) {
+    return (isNaN(ele * 1)) ? document.querySelector(ele) : document.getElementById(ele);
+  } if (mode === 'all') {
+    return (parent === undefined) ? document.querySelectorAll(ele) : parent.querySelectorAll(ele);
+  } if (typeof mode === 'object' && parent === undefined) {
+    return mode.querySelector(ele);
+  }
 }
 const top = gE('#hvut-top');
+const keys = ['.encounterUI', '.hvAAPauseUI', '.hvAAButton'];
+let done = 0;
 if(top) {
-    [gE('.encounterUI'),gE('.hvAAPauseUI'),gE('.hvAAButton')].forEach(ui=>{
-        if(ui) top.appendChild(ui);
-    })
+  appendUI();
+}
+
+function appendUI() {
+  let ui;
+  for(const key of keys) {
+    ui = gE(key);
+    if(!ui || !gE(key, top)) {
+      continue;
+    }
+    // top.removeChild(ui);
+    document.body.appendChild(ui);
+  }
+  for(const key of keys) {
+    ui = gE(key);
+    if(!ui || gE(key, top)) {
+      continue;
+    }
+    top.appendChild(ui);
+    done++;
+  }
+  if (done === keys.length) {
+    return;
+  }
+  setTimeout(appendUI, 1000);
 }
