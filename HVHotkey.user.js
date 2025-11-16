576a1be4362c515212962891650e6eef4bb88384
WayneFerdon <wayneferdon@hotmail.com>
2025-11-16 22:28:25 +0800

    Update HVHotkey script version and enhance functionality

--- HVHotkey.user.js
+++ HVHotkey.user.js
@@ -1,26 +1,35 @@
 // ==UserScript==
 // @name         HVHotkey
-// @version      2025.10.04.
+// @version      2025.11.16.
 // @author       WayneFerdon
 // @include      http*://*hentaiverse.org/*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @updateURL https://github.com/WayneFerdon/HVUT/raw/refs/heads/master/HVHotkey.user.js
 // ==/UserScript==
 
+const autoSellItems = [12101, 12201, 12301, 12401, 12501, 12601, 13101, 13111, 13201, 13211, 13221, 60001, 60002, 60003, 60004, 60007, 60010, 60011, 60012, 60051, 60101, 60105, 61001, 61101, 61501, 60402, 60412, 60422, 30016, 30017, 30018, 30019, 30020, 30021, 30022, 30023, 30024, 30025, 30026, 30027, 30028, 30029, 30030, 30031, 30032];
+
 if (window.self !== window.top) {
   return;
 }
-
-function gE(ele, mode, parent) { // 获取元素
-  if (typeof ele === 'object') return ele;
-  if (mode === undefined && parent === undefined) return (isNaN(ele * 1)) ? document.querySelector(ele) : document.getElementById(ele);
-  if (mode === 'all') return (parent === undefined) ? document.querySelectorAll(ele) : parent.querySelectorAll(ele);
-  if (typeof mode === 'object' && parent === undefined) return mode.querySelector(ele);
+const battle = gE('#battle_main');
+if (battle) {
+  handleBattle();
+  observe_node(document.querySelector('#textlog').firstChild, { childList: true }, handleBattle);
+  // 新回合开始时会刷新 battle_main，导致原本的监听失效，必须在刷新时重新监听一次
+  observe_node(document.querySelector('#battle_main'), { childList: true }, () => {
+    handleBattle();
+    observe_node(document.querySelector('#textlog').firstChild, { childList: true }, handleBattle);
+  });
+  observe_node(document.querySelector('body'), { childList: true }, () => {
+    handleBattle();
+    observe_node(document.querySelector('#textlog').firstChild, { childList: true }, handleBattle);
+  });
+  return;
 }
 
-function cE(name) { // 创建元素
-  return document.createElement(name);
-}
+// defaultNotSellSalvage();
+autoCheck();
 
 var _query = Object.fromEntries(location.search.slice(1).split('&').map((q) => { const [k, v = ''] = q.split('=', 2); return [k, decodeURIComponent(v.replace(/\+/g, ' '))]; }));
 
@@ -57,22 +66,26 @@ document.addEventListener('keydown', e => {
     [69, marketBuyOrder],//e
     [37, marketPrev],//←
     [39, marketNext],//→
+    [87, marketPrev],//w
+    [83, marketNext],//s
   ]
   if(keyCode !== 32) {
     for (let [key, selector] of handlers) {
       if (key === -1 || keyCode!==key) continue;
+      console.log(key);
       const ui = gE(selector);
+      if(ui?.href){
+        window.location.href = ui.href;
+        return;
+      }
       try{
-        gE(ui).click();
+        ui?.click();
+        return;
       }
       catch{
       }
-      if(ui.onclick) {
-        return;
-      }
-      if(!ui.href) return;
-      window.location.href = ui.href;
     }
+    return;
   }
   // keyCode === 32 => space
   event.preventDefault();
@@ -99,9 +112,7 @@ document.addEventListener('keydown', e => {
   if(stock){
     const item = _query.itemid*1;
     if(stock.innerHTML == 0 ||
-       ![12101, 12201, 12301, 12401, 12501, 12601, 13101, 13201, 13211, 13221,
-         60001, 60002, 60003, 60004, 60007, 60010, 60011, 60012, 60051, 60101, 60105, 61001, 61101, 61501, 60402, 60412, 60422,
-         30016, 30017, 30018, 30019, 30020, 30021, 30022, 30023, 30024, 30025, 30026, 30027, 30028, 30029, 30030, 30031, 30032].includes(item)){
+       !autoSellItems.includes(item)){
       gE(marketNext).click();
       return;
     }
@@ -117,18 +128,120 @@ document.addEventListener('keydown', e => {
   }
 
   // character
-  let incBtns = Array.from(gE('#attr_table img', 'all')).filter(e=>e.id.includes('_inc') && !e.src.includes('_d.png'));
-  while(incBtns.length){
-    let displays = Array.from(gE('#attr_table td', 'all')).filter(e=>e.id.includes('_display'));
-    displays = incBtns.map(e=>displays.find(d=>d.id === e.id.replace('_inc','_display')));
-    let min=Number.MAX_SAFE_INTEGER, minBtn;
-    for(let i = 0; i<displays.length; i++){
-      let lv = displays[i].innerText * 1;
+  let incBtns;
+  let isAdding;
+  while(!incBtns || incBtns.length){
+    incBtns = Array.from(gE('#attr_table img', 'all')).filter(e=>e.id.includes('_inc') && !e.src.includes('_d.png'));
+    let displays = incBtns.map(e=>Array.from(gE('#attr_table td', 'all')).filter(e=>e.id.includes('_display')).find(d=>d.id === e.id.replace('_inc','_display')));
+    let min=Number.MAX_SAFE_INTEGER;
+    minBtn = undefined;
+    let i;
+    for(i = 0; i<displays.length; i++){
+      let lv;
+      if(displays[i].innerText !== ''){
+        lv = displays[i].innerText * 1;
+      } else {
+        lv = 0;
+        let n = 0;
+        for(let img of gE('div>div', 'all', displays[i])){
+          lv = img.style.cssText.match(/0px (-*)(\d+)px/)[2] * 1 / 12 * (10 ** n++) + lv;
+        }
+      }
       if(lv >= min) continue;
       min = lv;
       minBtn = incBtns[i];
     }
-    minBtn.click();
-    incBtns = Array.from(gE('#attr_table img', 'all')).filter(e=>e.id.includes('_inc') && !e.src.includes('_d.png'));
+    minBtn?.click();
+    isAdding ??= minBtn;
+  }
+  if (isAdding){
+    gE(`img[src*="/y/character/apply.png"]`)?.onclick();
   }
 }, false);
+
+function gE(ele, mode, parent) { // 获取元素
+  if (typeof ele === 'object') return ele;
+  if (mode === undefined && parent === undefined) return (isNaN(ele * 1)) ? document.querySelector(ele) : document.getElementById(ele);
+  if (mode === 'all') return (parent === undefined) ? document.querySelectorAll(ele) : parent.querySelectorAll(ele);
+  if (typeof mode === 'object' && parent === undefined) return mode.querySelector(ele);
+}
+
+function cE(name) { // 创建元素
+  return document.createElement(name);
+}
+
+function defaultNotSellSalvage(){
+  const sellSalvage = gE('#sell_salvage');
+  if(!sellSalvage){
+    return;
+  }
+  const ls = gE('span', sellSalvage.parentNode);
+  const isSell = window.getComputedStyle(ls, '::after').display === 'block';
+  if(isSell){
+    sellSalvage.checked = false;
+  }
+}
+
+function autoCheck(){
+  if(!gE('#cfs1') || !gE('#cfs2')){
+    setTimeout(autoCheck, 1000);
+    return;
+  }
+  gE('#cfs1').checked = true;
+  gE('#cfs2').checked = true;
+  gE('#equipform').submit();
+  setTimeout(autoCheck, 1000);
+}
+
+function handleBattle() {
+  if (!gE('#pane_action>div>div')) {
+    gE('#pane_action').style.cssText += 'height: 40px;'
+    const actions = gE('#pane_action>div', 'all');
+    const keys = ['Q', 'W', 'E', 'S', 'D', 'F'];
+    for (const i in actions) {
+      if (isNaN(i * 1)) {
+        break;
+      }
+      const action = actions[i];
+      const key = cE('div');
+      key.style.cssText += 'color: black; font-weight: bolder; font-size: 18px;'
+      key.innerHTML = keys[i];
+      action.appendChild(key);
+    };
+  }
+  const pane_item = gE('#pane_item');
+  const gem = gE('.bti1>.bti2>div>div', pane_item);
+  if (!gem.innerText.includes('G/')) {
+    gem.innerText = 'G/' + gem.innerText;
+    const items = gE('.c>div:first-child>.c>.bti1>.bti2>div>div', 'all', pane_item);
+    items.forEach( item => { item.innerText = 'F' + item.innerText; });
+    const scrolls = gE('.c>div:nth-child(2)>.c>.bti1>.bti2>div>div', 'all', pane_item);
+    scrolls.forEach( scroll => { scroll.innerText = scroll.innerText.replace('S', '⇧+F').replace('N', 'Ctrl+F'); });
+  }
+  const quickbar = gE('#quickbar');
+  const skills = gE('.btsd', 'all');
+  if(!gE('#quickbar>div:not(.btqs)', quickbar)) {
+    for (let i = 0; i < 10; i++) {
+      const index = cE('div');
+      const hint = `alt${i === 9 ? 0 : i + 1}`;
+      index.style.cssText += `position:absolute; top: 115px; left: ${30 + i * 37}px; color: white; font-weight: bolder; font-size: 18px; z-index:1000`;
+      index.innerHTML = hint;
+      quickbar.appendChild(index);
+      ;
+      const onmousehoverText = quickbar.childNodes[i].getAttribute('onmouseover');
+      for (let skill of skills) {
+        if (skill.getAttribute('onmouseover') !== onmousehoverText) {
+          continue;
+        }
+        gE('div>div', skill).innerText = `[${hint}]` + gE('div>div', skill).innerText;
+        break;
+      }
+    }
+  }
+}
+
+function observe_node(node, config, callback) {
+  const observer = new MutationObserver(callback);
+  observer.observe(node, config);
+  return observer;
+}
